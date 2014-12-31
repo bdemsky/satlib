f43befe4da7618bfda6720be15e3804914c38ac7
bdemsky <bdemsky@uci.edu>
2014-12-31 17:11:17 +0900

    Incremental frontend for glucose solver

---
+++ glucose-syrup/incremental/depend.mk
@@ -0,0 +1,41 @@
+/Users/briandemsky/research/model/sat/glucose-syrup/incremental/Main.o /Users/briandemsky/research/model/sat/glucose-syrup/incremental/Main.or /Users/briandemsky/research/model/sat/glucose-syrup/incremental/Main.od /Users/briandemsky/research/model/sat/glucose-syrup/incremental/Main.op: \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/Main.cc \
+  ../../solver_interface.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../utils/System.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../mtl/IntTypes.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../utils/ParseUtils.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../utils/Options.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../mtl/Vec.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../mtl/XAlloc.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../core/Dimacs.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../core/SolverTypes.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../mtl/Alg.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../mtl/Map.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../mtl/Alloc.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../simp/SimpSolver.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../mtl/Queue.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../core/Solver.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../mtl/Heap.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../core/BoundedQueue.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../core/Constants.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../mtl/Clone.h
+/Users/briandemsky/research/model/sat/glucose-syrup/incremental/SimpSolver.o /Users/briandemsky/research/model/sat/glucose-syrup/incremental/SimpSolver.or /Users/briandemsky/research/model/sat/glucose-syrup/incremental/SimpSolver.od /Users/briandemsky/research/model/sat/glucose-syrup/incremental/SimpSolver.op: \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/SimpSolver.cc \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../mtl/Sort.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../mtl/Vec.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../mtl/IntTypes.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../mtl/XAlloc.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../simp/SimpSolver.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../mtl/Queue.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../core/Solver.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../mtl/Heap.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../mtl/Alg.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../utils/Options.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../utils/ParseUtils.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../core/SolverTypes.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../mtl/Map.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../mtl/Alloc.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../core/BoundedQueue.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../core/Constants.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../mtl/Clone.h \
+  /Users/briandemsky/research/model/sat/glucose-syrup/incremental/../utils/System.h
