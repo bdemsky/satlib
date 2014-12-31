5ac92c1329d2c3097f7d8c0c7adb19101b6d8414
bdemsky <bdemsky@uci.edu>
2014-12-31 22:17:49 +0900

    update test case

--- test_solver.cc
+++ test_solver.cc
@@ -6,11 +6,20 @@ int main(int argc, char **argv) {
   s->finishedClauses();
   s->freeze(1); s->freeze(2);
   printf("solution=%d\n", s->solve());
+  for(int i=0;i<=2;i++) {
+    printf("%d: %d\n",i, s->getValue(i));
+  }
   s->addClauseLiteral(-1);s->addClauseLiteral(0);
   s->finishedClauses();
   printf("solution=%d\n", s->solve());
+  for(int i=0;i<=2;i++) {
+    printf("%d: %d\n",i, s->getValue(i));
+  }
   s->addClauseLiteral(-2);s->addClauseLiteral(0);
   s->finishedClauses();
   printf("solution=%d\n", s->solve());
+  for(int i=0;i<=2;i++) {
+    printf("%d: %d\n",i, s->getValue(i));
+  }
   delete s;
 }
