16db0f7120215c74f5a4ab47e70b97df59bf7513
bdemsky <bdemsky@uci.edu>
2014-12-31 15:42:23 +0900

    factor out common constants

---
+++ solver_interface.h
@@ -0,0 +1,12 @@
+#ifndef SOLVER_INTERFACE_H
+#define SOLVER_INTERFACE_H
+
+#define IS_UNSAT 0
+#define IS_SAT 1
+#define IS_INDETER 2
+#define IS_RUNSOLVER 3
+#define IS_FREEZE 3
+
+#define BUFFERSIZE 1024
+
+#endif
